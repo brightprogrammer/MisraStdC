82a15dec79b84ea2d803b1f0f10db094bd17d7c8
Siddharth Mishra <admin@brightprogrammer.in>
2025-05-17 19:44:50 +0530

    Document Helper Macros

--- Include/Misra/Types.h
+++ Include/Misra/Types.h
@@ -26,70 +26,143 @@ typedef i8 bool;
 
 typedef unsigned long size;
 
+///
+/// Returns the smaller of two values `x` and `y`.
+///
+/// x[in] : First value for comparison.
+/// y[in] : Second value for comparison.
+///
+/// RETURN : The smaller of `x` and `y`.
+///
 #define MIN2(x, y) ((x) < (y) ? (x) : (y))
+
+///
+/// Returns the larger of two values `x` and `y`.
+///
+/// x[in] : First value for comparison.
+/// y[in] : Second value for comparison.
+///
+/// RETURN : The larger of `x` and `y`.
+///
 #define MAX2(x, y) ((x) > (y) ? (x) : (y))
 
-/// Make an lval usable with a pointer
-/// Pass any literal to this and then you can take it's address, like you do for a variable
+///
+/// Creates a temporary, addressable l-value from a given expression `x`.
+/// This allows taking the address of literals or other r-values as if they were
+/// variables.
+///
+/// x[in] : The expression whose value needs to be addressable.
+///
+/// RETURN : The value of `x` as the first element of a temporary array. This allows
+///          using the address-of operator (`&`) on the result (e.g., `&LVAL(123)`).
+///
 #define LVAL(x) ((__typeof__(x)[]) {(x)})[0]
 
 ///
-/// Clamp the value of `x` in between `lo` and `hi`
+/// Clamps the value of `x` to be within the inclusive range [`lo`, `hi`].
+/// If `x` is less than `lo`, it returns `lo`. If `x` is greater than `hi`, it returns `hi`.
+/// Otherwise, it returns `x`.
 ///
-/// x[in]  : Value to be clamped
-/// lo[in] : Range low value
-/// hi[in] : Range high value
+/// x[in]  : The value to be clamped.
+/// lo[in] : The lower bound of the clamping range.
+/// hi[in] : The upper bound of the clamping range.
 ///
-/// RETURN : clamp value
+/// RETURN : The clamped value of `x`.
 ///
 #define CLAMP(x, lo, hi) MIN2(MAX2(lo, x), hi)
 
-// for any general alignment value (13, 8, 17, 144, etc...)
+///
+/// Aligns the given `value` up to the nearest multiple of `alignment`.
+/// If `alignment` is 1 or less, the original `value` is returned.
+/// This works for any general alignment value.
+///
+/// value[in]     : The value to be aligned up.
+/// alignment[in] : The alignment boundary (must be greater than 0 for alignment to occur).
+///
+/// RETURN : The smallest multiple of `alignment` that is greater than or equal to `value`.
+///          Returns `value` if `alignment` is not greater than 1.
+///
 #define ALIGN_UP(value, alignment)                                                                                     \
     ((alignment) > 1 ? (((value) + (alignment) - 1) / (alignment) * (alignment)) : (value))
 
+///
+/// Aligns the given `value` down to the nearest multiple of `alignment`.
+/// If `alignment` is 1 or less, the original `value` is returned.
+/// This works for any general alignment value.
+///
+/// value[in]     : The value to be aligned down.
+/// alignment[in] : The alignment boundary (must be greater than 0 for alignment to occur).
+///
+/// RETURN : The largest multiple of `alignment` that is less than or equal to `value`.
+///          Returns `value` if `alignment` is not greater than 1.
+///
 #define ALIGN_DOWN(value, alignment) ((alignment) > 1 ? ((value) / (alignment) * (alignment)) : (value))
 
-// for alignment value that is power of two, (2, 4, 8, 16, 32, ...)
+///
+/// Aligns the given `value` up to the nearest power-of-two multiple of `alignment`.
+/// It is assumed that `alignment` is a power of two. If `alignment` is 1 or less,
+/// the original `value` is returned.
+///
+/// value[in]     : The value to be aligned up.
+/// alignment[in] : The power-of-two alignment boundary (must be greater than 0 for
+///                 alignment to occur and should be a power of two).
+///
+/// RETURN : The smallest power-of-two multiple of `alignment` that is greater than
+///          or equal to `value`. Returns `value` if `alignment` is not greater than 1.
+///
 #define ALIGN_UP_POW2(value, alignment) ((alignment) > 1 ? (((value) + (alignment) - 1) & ~((alignment) - 1)) : (value))
 
+///
+/// Aligns the given `value` down to the nearest power-of-two multiple of `alignment`.
+/// It is assumed that `alignment` is a power of two. If `alignment` is 1 or less,
+/// the original `value` is returned.
+///
+/// value[in]     : The value to be aligned down.
+/// alignment[in] : The power-of-two alignment boundary (must be greater than 0 for
+///                 alignment to occur and should be a power of two).
+///
+/// RETURN : The largest power-of-two multiple of `alignment` that is less than or
+///          equal to `value`. Returns `value` if `alignment` is not greater than 1.
+///
 #define ALIGN_DOWN_POW2(value, alignment) ((alignment) > 1 ? ((value) & ~((alignment) - 1)) : (value))
 
 ///
-/// Is `x` in inclusive range of `hi` and `lo`
+/// Checks if the value `x` is within the inclusive range [`lo`, `hi`].
 ///
-/// x[in]  : Value to check in range for
-/// lo[in] : Range low value
-/// hi[in] : Range high value
+/// x[in]  : The value to check.
+/// lo[in] : The lower bound of the range.
+/// hi[in] : The upper bound of the range.
 ///
-/// RETURN : true/false depending on whether value is in range
+/// RETURN : `true` (non-zero) if `x` is greater than or equal to `lo` and less than
+///          or equal to `hi`. `false` (zero) otherwise.
 ///
-#define IN_RANGE(x, lo, hi) ((x) >= (hi) && (x) <= (lo))
+#define IN_RANGE(x, lo, hi) ((x) >= (lo) && (x) <= (hi))
 
 ///
-/// Is given character a capital ASCII alphabet?
+/// Checks if the given character `c` is an uppercase ASCII alphabet (A-Z).
 ///
-/// c[in] : Character to check for
+/// c[in] : The character to check.
 ///
-/// RETURN : true/false depending on whether `c` is a capital alphabet or not
+/// RETURN : `true` (non-zero) if `c` is an uppercase ASCII alphabet. `false` (zero) otherwise.
 ///
 #define IS_CAPS_ALPHA(c) IN_RANGE(c, 'A', 'Z')
 
 ///
-/// Is given character an ASCII alphabet?
+/// Checks if the given character `c` is an ASCII alphabet (a-z or A-Z).
 ///
-/// c[in] : Character to check for
+/// c[in] : The character to check.
 ///
-/// RETURN : true/false depending on whether `c` is an alphabet or not
+/// RETURN : `true` (non-zero) if `c` is a lowercase or uppercase ASCII alphabet.
+///          `false` (zero) otherwise.
 ///
 #define IS_ALPHA(c) (IN_RANGE(c, 'a', 'z') || IN_RANGE(c, 'A', 'Z'))
 
 ///
-/// Is given character an ASCII digit?
+/// Checks if the given character `c` is an ASCII digit (0-9).
 ///
-/// c[in] : Character to check for
+/// c[in] : The character to check.
 ///
-/// RETURN : true/false depending on whether `c` is a digit or not
+/// RETURN : `true` (non-zero) if `c` is an ASCII digit. `false` (zero) otherwise.
 ///
 #define IS_DIGIT(c) IN_RANGE(c, '0', '9')
 
