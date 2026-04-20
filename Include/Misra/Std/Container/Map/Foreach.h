ff489445506473d9cec39ad22cdc5bf513a72b3e
Siddharth Mishra <admin@brightprogrammer.in>
2026-04-19 18:08:13 -0700

    Remove public Map slot-index iterators

--- Include/Misra/Std/Container/Map/Foreach.h
+++ Include/Misra/Std/Container/Map/Foreach.h
@@ -10,56 +10,36 @@
 #include "Access.h"
 
 ///
-/// Iterate over occupied key/value pairs with pointers and explicit slot index.
-///
-/// TAGS: Map, Foreach, Pointer, Index
-///
-#define MapForeachPtrIdx(m, key_ptr, value_ptr, idx)                                                              \
-    for (TYPE_OF(m) UNPL(pm) = (m); UNPL(pm); UNPL(pm) = NULL)                                                        \
-        if ((ValidateMap(UNPL(pm)), 1) && UNPL(pm)->length > 0)                                                   \
-            for (size idx = 0; idx < UNPL(pm)->capacity; idx++)                                                       \
-                if (MapSlotOccupied(UNPL(pm), idx))                                                                \
-                    for (bool UNPL(_once) = true; UNPL(_once); UNPL(_once) = false)                                   \
-                        for (MAP_KEY_TYPE(UNPL(pm)) *key_ptr = MapKeyPtrAt(UNPL(pm), idx);                    \
-                             UNPL(_once);                                                                              \
-                             UNPL(_once) = false)                                                                      \
-                            for (MAP_VALUE_TYPE(UNPL(pm)) *value_ptr = MapValuePtrAt(UNPL(pm), idx);          \
-                                 value_ptr;                                                                            \
-                                 value_ptr = NULL)
-
-///
 /// Iterate over occupied key/value pairs with pointers.
 ///
 /// TAGS: Map, Foreach, Pointer
 ///
-#define MapForeachPtr(m, key_ptr, value_ptr) MapForeachPtrIdx((m), (key_ptr), (value_ptr), UNPL(iter))
-
-///
-/// Iterate over occupied key/value pairs by value with explicit slot index.
-///
-/// TAGS: Map, Foreach, Index
-///
-#define MapForeachIdx(m, key_var, value_var, idx)                                                                 \
-    MapForeachIdx_IMPL((m), (key_var), (value_var), idx, UNPL(map_once))
-
-#define MapForeachIdx_IMPL(m, key_var, value_var, idx, once_var)                                                  \
-    for (TYPE_OF(m) UNPL(pm) = (m); UNPL(pm); UNPL(pm) = NULL)                                                        \
-        if ((ValidateMap(UNPL(pm)), 1) && UNPL(pm)->length > 0)                                                   \
-            for (size idx = 0; idx < UNPL(pm)->capacity; idx++)                                                       \
-                if (MapSlotOccupied(UNPL(pm), idx))                                                                \
-                    for (bool once_var = true; once_var; once_var = false)                                             \
-                        for (MAP_KEY_TYPE(UNPL(pm)) key_var = *MapKeyPtrAt(UNPL(pm), idx);                    \
-                             once_var;                                                                                 \
-                             once_var = false)                                                                         \
-                            for (MAP_VALUE_TYPE(UNPL(pm)) value_var = *MapValuePtrAt(UNPL(pm), idx);          \
-                                 once_var;                                                                             \
-                                 once_var = false)
+#define MapForeachPtr(m, key_ptr, value_ptr)                                                                                               \
+    for (TYPE_OF(m) UNPL(pm) = (m); UNPL(pm); UNPL(pm) = NULL)                                                                             \
+        if ((ValidateMap(UNPL(pm)), 1) && UNPL(pm)->length > 0)                                                                            \
+            for (size UNPL(slot) = 0; UNPL(slot) < UNPL(pm)->capacity; UNPL(slot)++)                                                       \
+                if (MapSlotOccupied(UNPL(pm), UNPL(slot)))                                                                                 \
+                    for (bool UNPL(_once) = true; UNPL(_once); UNPL(_once) = false)                                                        \
+                        for (MAP_KEY_TYPE(UNPL(pm)) *key_ptr = MapKeyPtrAt(UNPL(pm), UNPL(slot)); UNPL(_once);                             \
+                             UNPL(_once)                     = false)                                                                      \
+                            for (MAP_VALUE_TYPE(UNPL(pm)) *value_ptr = MapValuePtrAt(UNPL(pm), UNPL(slot)); value_ptr; \
+                                 value_ptr                           = NULL)
 
 ///
 /// Iterate over occupied key/value pairs by value.
 ///
 /// TAGS: Map, Foreach
 ///
-#define MapForeach(m, key_var, value_var) MapForeachIdx((m), (key_var), (value_var), UNPL(iter))
+#define MapForeach(m, key_var, value_var)                                                                                      \
+    for (TYPE_OF(m) UNPL(pm) = (m); UNPL(pm); UNPL(pm) = NULL)                                                                 \
+        if ((ValidateMap(UNPL(pm)), 1) && UNPL(pm)->length > 0)                                                                \
+            for (size UNPL(slot) = 0; UNPL(slot) < UNPL(pm)->capacity; UNPL(slot)++)                                           \
+                if (MapSlotOccupied(UNPL(pm), UNPL(slot)))                                                                     \
+                    for (bool UNPL(_once) = true; UNPL(_once); UNPL(_once) = false)                                            \
+                        for (MAP_KEY_TYPE(UNPL(pm)) key_var = *MapKeyPtrAt(UNPL(pm), UNPL(slot)); UNPL(_once);                 \
+                             UNPL(_once)                    = false)                                                           \
+                            for (MAP_VALUE_TYPE(UNPL(pm)) value_var = *MapValuePtrAt(UNPL(pm), UNPL(slot)); \
+                                 UNPL(_once);                                                               \
+                                 UNPL(_once) = false)
 
 #endif // MISRA_STD_CONTAINER_MAP_FOREACH_H
