5e630507a33a1021e702cfecc7725cb78232ac3b
Siddharth Mishra <admin@brightprogrammer.in>
2026-05-13 19:12:34 -0700

    Graph/Insert: convert MustAddNode L/R to do-while with LOG_FATAL
    The Must variants now follow the project rule that XXXMustYYY
    forms are statement-style do-while wrappers around the propagating
    expression form. Drop the graph_require_inserted_node and
    graph_abort_insert_operation helpers and the GRAPH_ABORT macro;
    inline LOG_FATAL instead so the failure path stays at the call site.

--- Include/Misra/Std/Container/Graph/Insert.h
+++ Include/Misra/Std/Container/Graph/Insert.h
@@ -10,10 +10,6 @@
 #include "Type.h"
 #include "Private.h"
 
-#include <stdio.h>
-
-void SysAbort(void);
-
 #if defined(MISRA_ENFORCE_TYPE_SAFETY) && MISRA_ENFORCE_TYPE_SAFETY
 #    define GRAPH_TYPECHECK_NODE_L(g, node) ((void)sizeof(char[_Generic(&(node), GRAPH_NODE_TYPE(g) * : 1, default : -1)]))
 #    define GRAPH_TYPECHECK_NODE_R(g, node) ((void)sizeof((GRAPH_NODE_TYPE(g)[]){(node)}))
@@ -21,21 +17,6 @@ void SysAbort(void);
 #    define GRAPH_TYPECHECK_NODE_L(g, node) ((void)0)
 #    define GRAPH_TYPECHECK_NODE_R(g, node) ((void)0)
 #endif
-
-#define GRAPH_ABORT(message) graph_abort_insert_operation(__func__, __LINE__, (message))
-
-static inline void graph_abort_insert_operation(const char *function, int line, const char *message) {
-    fprintf(stderr, "FATAL [%s:%d] %s\n", function, line, message);
-    SysAbort();
-}
-
-static inline GraphNodeId graph_require_inserted_node(GraphNodeId node_id, const char *message) {
-    if (!node_id) {
-        GRAPH_ABORT(message);
-    }
-
-    return node_id;
-}
 
 #define GraphAddNodeL(g, lval)                                                                                         \
     (ValidateGraph(g),                                                                                                \
@@ -51,16 +32,26 @@ static inline GraphNodeId graph_require_inserted_node(GraphNodeId node_id, const
 
 #define GraphAddEdge(g, from, to) (ValidateGraph(g), graph_add_edge(GENERIC_GRAPH(g), (from), (to)))
 
-#define GraphMustAddNodeL(g, lval) graph_require_inserted_node(GraphAddNodeL((g), (lval)), "GraphMustAddNodeL failed")
+#define GraphMustAddNodeL(g, lval)                                                                                     \
+    do {                                                                                                               \
+        if (!GraphAddNodeL((g), (lval))) {                                                                             \
+            LOG_FATAL("GraphMustAddNodeL failed");                                                                     \
+        }                                                                                                              \
+    } while (0)
 
-#define GraphMustAddNodeR(g, rval) graph_require_inserted_node(GraphAddNodeR((g), (rval)), "GraphMustAddNodeR failed")
+#define GraphMustAddNodeR(g, rval)                                                                                     \
+    do {                                                                                                               \
+        if (!GraphAddNodeR((g), (rval))) {                                                                             \
+            LOG_FATAL("GraphMustAddNodeR failed");                                                                     \
+        }                                                                                                              \
+    } while (0)
 
 #define GraphMustAddNode(g, lval) GraphMustAddNodeL((g), (lval))
 
 #define GraphMustAddEdge(g, from, to)                                                                                  \
     do {                                                                                                               \
         if (!GraphAddEdge((g), (from), (to))) {                                                                        \
-            GRAPH_ABORT("GraphMustAddEdge failed");                                                                    \
+            LOG_FATAL("GraphMustAddEdge failed");                                                                      \
         }                                                                                                              \
     } while (0)
 
