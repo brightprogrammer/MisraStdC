e2516a1894f8850dedd5f7cb6032c537aafb6e0a
Siddharth Mishra <admin@brightprogrammer.in>
2026-05-15 17:08:09 -0700

    log: drop time/localtime_r/strftime, use in-tree UTC formatter
    LogInit (redirect=true mode) was using time() + localtime_r() +
    strftime("%Y-%m-%d-%H-%M-%S") to build the per-run log file name.
    Replace with a small in-tree formatter:
    
      - misra_log_epoch_seconds: direct clock_gettime(CLOCK_REALTIME)
        syscall on Linux; libc time() on macOS / Windows where direct
        syscalls aren't sanctioned.
      - misra_log_format_utc: Howard Hinnant's proleptic-Gregorian
        civil_from_days algorithm. Produces "YYYY-MM-DD-HH-MM-SS" in
        UTC. The log file name no longer reflects local time, but the
        file is unique per second either way.
    
    Drops localtime_r + strftime from the undefined-symbol list (time()
    is still pulled in only on macOS/Windows; the Linux build is clean).

--- Source/Misra/Std/Log.c
+++ Source/Misra/Std/Log.c
@@ -12,6 +12,80 @@
 #include <Misra/Std/Allocator/Default.h>
 #include <Misra/Sys.h>
 
+#include "../_Syscall.h"
+
+#include <stdint.h>
+
+// Get the current UTC epoch seconds without libc `time()`. Linux:
+// direct clock_gettime(CLOCK_REALTIME); macOS / Windows fall back to
+// libc time() which is part of libSystem / UCRT (not glibc).
+static u64 misra_log_epoch_seconds(void) {
+#if MISRA_HAVE_DIRECT_SYSCALL
+    struct {
+        long sec;
+        long nsec;
+    } ts     = {0, 0};
+    long ret = misra_sys2(MISRA_SYS_clock_gettime, 0L /* CLOCK_REALTIME */, (long)(uintptr_t)&ts);
+    if (ret < 0) {
+        return 0;
+    }
+    return (u64)ts.sec;
+#else
+    time_t t = time(NULL);
+    return (u64)t;
+#endif
+}
+
+// In-tree UTC-time formatter producing "YYYY-MM-DD-HH-MM-SS".
+// Replaces libc localtime_r + strftime so the log subsystem doesn't
+// drag the locale / TZ machinery in. `out` must hold at least 20
+// chars (19 + NUL). Uses Howard Hinnant's proleptic-Gregorian
+// algorithm for days-since-epoch -> y/m/d.
+static void misra_log_format_utc(u64 epoch_secs, char out[20]) {
+    u64 days  = epoch_secs / 86400u;
+    u64 dsecs = epoch_secs % 86400u;
+    u32 hour  = (u32)(dsecs / 3600);
+    u32 min   = (u32)((dsecs / 60) % 60);
+    u32 sec   = (u32)(dsecs % 60);
+
+    // Hinnant: civil_from_days
+    i64 z   = (i64)days + 719468; // shift origin to 0000-03-01
+    i64 era = (z >= 0 ? z : z - 146096) / 146097;
+    u32 doe = (u32)(z - era * 146097);
+    u32 yoe = (doe - doe / 1460 + doe / 36524 - doe / 146096) / 365;
+    i64 y   = (i64)yoe + era * 400;
+    u32 doy = doe - (365 * yoe + yoe / 4 - yoe / 100);
+    u32 mp  = (5 * doy + 2) / 153;
+    u32 d   = doy - (153 * mp + 2) / 5 + 1;
+    u32 m   = mp < 10 ? mp + 3 : mp - 9;
+    if (m <= 2) {
+        y += 1;
+    }
+
+    // Format: YYYY-MM-DD-HH-MM-SS (19 chars + NUL).
+    u32 year = (u32)(y < 0 ? 0 : y);
+    out[0]   = (char)('0' + ((year / 1000) % 10));
+    out[1]   = (char)('0' + ((year / 100) % 10));
+    out[2]   = (char)('0' + ((year / 10) % 10));
+    out[3]   = (char)('0' + (year % 10));
+    out[4]   = '-';
+    out[5]   = (char)('0' + (m / 10));
+    out[6]   = (char)('0' + (m % 10));
+    out[7]   = '-';
+    out[8]   = (char)('0' + (d / 10));
+    out[9]   = (char)('0' + (d % 10));
+    out[10]  = '-';
+    out[11]  = (char)('0' + (hour / 10));
+    out[12]  = (char)('0' + (hour % 10));
+    out[13]  = '-';
+    out[14]  = (char)('0' + (min / 10));
+    out[15]  = (char)('0' + (min % 10));
+    out[16]  = '-';
+    out[17]  = (char)('0' + (sec / 10));
+    out[18]  = (char)('0' + (sec % 10));
+    out[19]  = '\0';
+}
+
 static FILE      *stderror             = NULL;
 static Mutex     *log_mutex            = NULL;
 static Allocator *log_persistent_alloc = NULL;
@@ -33,28 +107,8 @@ void LogInit(bool redirect, Allocator *alloc) {
         return;
     }
 
-    time_t    raw_time;
-    struct tm time_info;
-    char      time_buffer[20] = {0};
-
-    time(&raw_time);
-#ifdef _WIN32
-    if (localtime_s(&time_info, &raw_time)) {
-#else
-    if (!localtime_r(&raw_time, &time_info)) {
-#endif
-        DefaultAllocator err_alloc = DefaultAllocatorInit();
-        Str              syserr;
-        StrInitStack(syserr, &err_alloc, SYS_ERROR_STR_MAX_LENGTH, {
-            StrError(errno, &syserr);
-            LOG_ERROR("Failed to get localtime : {}", syserr);
-        });
-        DefaultAllocatorDeinit(&err_alloc);
-        LOG_SYS_ERROR("Failed to get localtime");
-        stderror = stderr;
-        return;
-    }
-    strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d-%H-%M-%S", &time_info);
+    char time_buffer[20] = {0};
+    misra_log_format_utc(misra_log_epoch_seconds(), time_buffer);
 
     DefaultAllocator init_alloc = DefaultAllocatorInit();
     Str              log_dir    = StrInit(&init_alloc);
